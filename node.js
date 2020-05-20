c77e02cbab214a2927a82d5e31a2745f2826a0dc
Inklud AS <61893998+Inklud@users.noreply.github.com>
2020-05-20 20:32:34 +0200

    Added additional fields
    Added sender from name and subject field.

--- node.js
+++ node.js
@@ -1,67 +1,89 @@
-var sgMail = require('@sendgrid/mail');
-var fileType = require('file-type');
+var sgMail = require("@sendgrid/mail");
+var fileType = require("file-type");
 
-module.exports = function (RED) {
-    'use strict';
-    function SendGridNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-        node.on('input', function (msg, send, done) {
-            send = send || function () {
-                node.send.apply(node, arguments);
-            };
-            node.status({fill: "blue", shape: "dot", text: "sendgrid.status.sending"});
-            var body;
-            var data = {
-                from: config.from || msg.from,
-                to: (to => Array.isArray(to) ? to : to.split(/[,; ]+/g))(msg.to || config.to || ''),
-                cc: (msg.cc || '').split(/[,; ]+/g),
-                bcc: (msg.bcc || '').split(/[,; ]+/g),
-                subject: msg.topic || msg.title || 'Message from Node-RED',
-                templateId: config.templateId || msg.templateId,
-                dynamic_template_data: (data => typeof data === 'object' ? data : JSON.parse(data))(config.templateData || msg.templateData || '{}'),
-            };
-            if(!config.templateId) {
-                if (Buffer.isBuffer(msg.payload)) {
-                    data.attachments = [{
-                        content: msg.payload.toString('base64'),
-                        filename: msg.filename || "attachment." + fileType(msg.payload).ext
-                    }];
-                    body = msg.description || " ";
-                } else {
-                    body = msg.payload.toString();
-                }
-
-                if (config.content === "html") {
-                    data.html = body;
-                } else {
-                    data.text = body;
-                }
+module.exports = function(RED) {
+  "use strict";
+  function SendGridNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.on("input", function(msg, send, done) {
+      send =
+        send ||
+        function() {
+          node.send.apply(node, arguments);
+        };
+      node.status({
+        fill: "blue",
+        shape: "dot",
+        text: "sendgrid.status.sending"
+      });
+      var body;
+      var data = {
+        from: {
+          email: config.from || msg.from,
+          name: config.fromname || msg.fromname
+        },
+        to: (to => (Array.isArray(to) ? to : to.split(/[,; ]+/g)))(
+          msg.to || config.to || ""
+        ),
+        cc: (msg.cc || "").split(/[,; ]+/g),
+        bcc: (msg.bcc || "").split(/[,; ]+/g),
+        subject:
+          config.subject || msg.title || msg.topic || "Message from Node-RED",
+        templateId: config.templateId || msg.templateId,
+        dynamic_template_data: (data =>
+          typeof data === "object" ? data : JSON.parse(data))(
+          config.templateData || msg.templateData || "{}"
+        )
+      };
+      if (!config.templateId) {
+        if (Buffer.isBuffer(msg.payload)) {
+          data.attachments = [
+            {
+              content: msg.payload.toString("base64"),
+              filename:
+                msg.filename || "attachment." + fileType(msg.payload).ext
             }
-
-            sgMail.setApiKey(node.credentials.key);
-            sgMail.send(data, config.multiple, function (err) {
-                if (err) {
-                    node.status({fill: "red", shape: "ring", text: "sendgrid.status.sendfail"});
-                    if (done) {
-                        done(err.toString());
-                    } else {
-                        node.error(err.toString(), msg);
-                    }
-                } else {
-                    if (done) {
-                        done();
-                    }
-                    setTimeout(function () {
-                        node.status({});
-                    }, 1000);
-                }
-            });
-        });
-    }
-    RED.nodes.registerType("sendgrid", SendGridNode, {
-        credentials: {
-            key: {type: "password"}
+          ];
+          body = msg.description || " ";
+        } else {
+          body = msg.payload.toString();
         }
+
+        if (config.content === "html") {
+          data.html = body;
+        } else {
+          data.text = body;
+        }
+      }
+
+      sgMail.setApiKey(node.credentials.key);
+      sgMail.send(data, config.multiple, function(err) {
+        if (err) {
+          node.status({
+            fill: "red",
+            shape: "ring",
+            text: "sendgrid.status.sendfail"
+          });
+          if (done) {
+            done(err.toString());
+          } else {
+            node.error(err.toString(), msg);
+          }
+        } else {
+          if (done) {
+            done();
+          }
+          setTimeout(function() {
+            node.status({});
+          }, 1000);
+        }
+      });
     });
+  }
+  RED.nodes.registerType("sendgrid", SendGridNode, {
+    credentials: {
+      key: { type: "password" }
+    }
+  });
 };
